106105e5117adf9dcca0e21a3a1121039b5d9003
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-08-03 01:47:14 -0400

    Robert Hogan sprint completed.

--- djorg/settings.py
+++ djorg/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS=['localhost', '127.0.0.1', 'robert-hogan-djorg.herokuapp.com']
-#testing again
+#completed.
 
 
 # Application definition
